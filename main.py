e33bb5a1880a8dd7e4ac19e139311c28890673aa
minhkakart <nguyenminh2732@gmail.com>
2023-10-24 08:59:06 +0700

    add predict form

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ from sklearn.preprocessing import LabelEncoder
 VIEW_WIDTH = 1920
 VIEW_HEIGHT = 1080
 WIDTH = 1340
-HEIGHT = 720
+HEIGHT = 900
 CENTER_X = (VIEW_WIDTH - WIDTH)/2
 CENTER_Y = (VIEW_HEIGHT - HEIGHT)/2
-x_max = 15
-y_max = 50
+global x_max
+global y_max
+global KMeans_clustering
+global file_name
 
-# KMeans_clustering = KMeans()
+x_max = 10
+y_max = 50
 
 dpg.create_context()
 
 def callback_selected_file(_, app_data):
-    # print("Sender: ", sender)
-    # print("App Data: ", app_data)
     selection = app_data['selections']
     file_name = list(selection.keys())
     if len(file_name) > 0:
@@ -30,18 +31,10 @@ def callback_selected_file(_, app_data):
             if(dpg.get_value(is_drop_first_col)):
                 data = data.iloc[:,1:]
             data = data.apply(LabelEncoder().fit_transform)
-            print('read ok')
             train,test = train_test_split(data, test_size=0.1,shuffle=False)
-            print('Split ok')
             KMeans_clustering = KMeans(n_clusters=dpg.get_value(inp_number_of_group),algorithm='elkan',n_init='auto')
-            print('Clustering ok')
             KMeans_clustering.fit(train)
-            print('fit ok')
 
-            # print('Centers:',KMeans_clustering.cluster_centers_)
-            # print(type(KMeans_clustering.cluster_centers_))
-
-            dpg.set_item_label(btn_select_file, file_name[0])
 
             labels = KMeans_clustering.labels_.tolist()
             # print(labels)
@@ -52,7 +45,7 @@ def callback_selected_file(_, app_data):
             x_max = len(gr)
             y_max = max(count)+20
 
-            label_pair = tuple(map(lambda item: ('Group ' + str(item), item), gr))
+            label_pair = tuple(map(lambda item: (str(item), item), gr))
             dpg.set_axis_ticks(xAxis, label_pair)
 
             # add series to y axis
@@ -60,12 +53,20 @@ def callback_selected_file(_, app_data):
             dpg.set_axis_limits(yAxis, 0, y_max)
             
             dpg.set_value(item='bar_series_tag',value=(gr,count))
-            # for i in gr:
-            #     dpg.draw_text((i-0.2,count[i]+10),str(count[i]),size=0.3,parent=kmean_plot)
+            dpg.delete_item(item=kmean_plot, children_only=True, slot=2)
+            for i in gr:
+                dpg.draw_text((i-0.2,count[i]+10),str(count[i]),size=0.3,parent=kmean_plot)
 
             ## score
-            dpg.set_value(item='score_bar_series',value=([1, 3],[silhouette_score(train, KMeans_clustering.labels_),davies_bouldin_score(train, KMeans_clustering.labels_)]))
+            score = [silhouette_score(train, KMeans_clustering.labels_), davies_bouldin_score(train, KMeans_clustering.labels_)]
+            dpg.set_value(item='score_bar_series', value=([1, 3],score))
+            dpg.delete_item(item='score_plot', children_only=True, slot=2)
+            for i, val in enumerate(score):
+                dpg.draw_text((i*2+0.7,val+0.19),str(int(val*10000)/10000),size=0.2,parent='score_plot')
+            # dpg.draw_text((1,0.5),text='test',size=0.2,parent='score_plot')
 
+
+            dpg.set_item_label(btn_select_file, file_name[0])
         except:
             dpg.set_item_label(btn_select_file, 'File is not supported!')
     else:
@@ -76,11 +77,16 @@ with dpg.file_dialog(directory_selector=False, show=False, callback=callback_sel
                       tag="file_dialog_tag", width=700 ,height=400):
     dpg.add_file_extension(".*")
 
+with dpg.window(label='Predict group of customer', show=False, tag='predict_window', pos=(200,100), width=980, height=550):
+    dpg.add_button(label='Predict')
+
 with dpg.window(label="Tutorial", width=WIDTH, height=HEIGHT,pos=(CENTER_X, CENTER_Y), no_scrollbar=True, tag="Primary Window"):
     btn_select_file = dpg.add_button(label="Select File", callback=lambda: dpg.show_item("file_dialog_tag"), pos=(10, 5))
     inp_number_of_group = dpg.add_input_int(label='Number of group', min_value=2, max_value=100, default_value=6, 
                                             pos=(400, 5), min_clamped=True, max_clamped=True, width=100)
     is_drop_first_col = dpg.add_checkbox(label='Drop first column',default_value=True, pos=(700, 5))
+
+    predict_btn = dpg.add_button(label='Predict a customer', pos=(10, 440), callback=lambda: dpg.show_item('predict_window'))
 
 
     # Plot group
@@ -97,10 +103,10 @@ with dpg.window(label="Tutorial", width=WIDTH, height=HEIGHT,pos=(CENTER_X, CENT
     with dpg.plot(label='Score', height=360, width=640, pos=(680,40), tag='score_plot'):
         dpg.add_plot_axis(dpg.mvXAxis)
         dpg.set_axis_ticks(dpg.last_item(), (('Silhouette score', 1),('Davies-Bouldin score', 3)))
-        # dpg.set_axis_limits(dpg.last_item(), 0, 4)
+        dpg.set_axis_limits(dpg.last_item(), 0, 4)
 
         dpg.add_plot_axis(dpg.mvYAxis, label='Score (minium is good)',tag='score_y_axis')
-        # dpg.set_axis_limits(dpg.last_item(), 0, 1.1)
+        dpg.set_axis_limits(dpg.last_item(), 0, 2)
         dpg.add_bar_series([],[],parent='score_y_axis', tag='score_bar_series')
 
 with dpg.theme() as global_theme:
@@ -117,7 +123,8 @@ with dpg.theme() as global_theme:
         dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5)
 
 dpg.bind_theme(global_theme)
-dpg.create_viewport(title='Custom Title', width=WIDTH, height=HEIGHT, x_pos=int(CENTER_X), y_pos=int(CENTER_Y), clear_color=(106, 176, 222, 255))
+# dpg.show_item_registry()
+dpg.create_viewport(title='Credit Card Dataset for Clustering', width=WIDTH, height=HEIGHT, x_pos=int(CENTER_X), y_pos=int(CENTER_Y), clear_color=(106, 176, 222, 255))
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.set_primary_window("Primary Window", True)
